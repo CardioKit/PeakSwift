c8d62efe3ae92e0c96e6bb0bff1ebe34c00cd9bc
Nikita <nikita.charush@gmail.com>
2023-06-19 14:56:09 +0200

    refactor(package interface): make enums lowercase

--- Sources/PeakSwift/Context/AlgorithmStrategy.swift
+++ Sources/PeakSwift/Context/AlgorithmStrategy.swift
@@ -18,19 +18,19 @@ class AlgorithmStrategy {
     
     func setAlgorithm(algorithm: Algorithms) {
         switch algorithm {
-        case .Basic:
+        case .basic:
             self.algorithm = Basic()
-        case .Aristotle:
+        case .aristotle:
             self.algorithm = Aristotle()
-        case .Christov:
+        case .christov:
             self.algorithm = Christov()
-        case .GQRS:
+        case .gqrs:
             self.algorithm = GQRS()
-        case .WQRS:
+        case .wqrs:
             self.algorithm = WQRS()
-        case .Nabian2018:
+        case .nabian2018:
             self.algorithm = Nabian2018()
-        case .Hamilton:
+        case .hamilton:
             self.algorithm = Hamilton()
         }
     }
