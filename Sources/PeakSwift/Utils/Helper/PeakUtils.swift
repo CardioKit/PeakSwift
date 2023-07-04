dbe15a88b8189bbcc59a038ee9b6fcfa2f43c5b5
NikitaChar <nikita.charush@gmail.com>
2023-07-04 13:59:41 +0200

    feature(PeakUtils): Add findPeakProminences

--- Sources/PeakSwift/Utils/Helper/PeakUtils.swift
+++ Sources/PeakSwift/Utils/Helper/PeakUtils.swift
@@ -9,6 +9,40 @@ import Foundation
 
 class PeakUtils {
     
+    struct Peaks {
+        let peaks: [Peak]
+        
+        var peakPosition: [Int] {
+            peaks.map {
+                peakAndProminence in
+                peakAndProminence.peak
+            }
+        }
+        
+        var peakProminences: [Double] {
+            peaks.map {
+                peakAndProminence in
+                peakAndProminence.prominence
+            }
+        }
+    }
+    
+    struct Peak {
+        
+        let peak: Int
+        let prominence: Double
+    }
+    
+    static func findAllPeaksAndProminences(signal: [Double]) -> Peaks {
+        let localMaxima = findAllLocalMaxima(signal: signal)
+        let prominences = findAllPeakProminences(signal: signal, peaks: localMaxima)
+        let peakAndPromineces =  zip(localMaxima, prominences).map {
+            (localmaximum, prominence) in
+            Peak(peak: localmaximum, prominence: prominence)
+        }
+        return Peaks(peaks: peakAndPromineces)
+    }
+    
     
     static func findAllLocalMaxima(signal: [Double]) -> [Int] {
         
@@ -40,5 +74,45 @@ class PeakUtils {
         }
         
         return peaks
+    }
+    
+    static func findAllPeakProminences(signal: [Double], peaks: [Int]) -> [Double] {
+        var prominences: [Double] = []
+        
+        
+        let minRange = 0
+        let maxRange = signal.count
+        
+        
+        for peakPos in 0..<peaks.count {
+            let peak = peaks[peakPos]
+            
+            var iLeft = peak
+            var leftMin = signal[peak]
+            
+            while minRange <= iLeft && signal[iLeft] <= signal[peak] {
+                if signal[iLeft] < leftMin {
+                    leftMin = signal[iLeft]
+                }
+                iLeft -= 1
+            }
+            
+            var iRight = peak
+            var rightMin = signal[peak]
+            
+            while iRight < maxRange && signal[iRight] <= signal[peak] {
+                if signal[iRight] < rightMin {
+                    rightMin = signal[iRight]
+                }
+                
+                iRight += 1
+            }
+            
+            let prominence = signal[peak] - max(leftMin, rightMin)
+            prominences.append(prominence)
+            
+        }
+        
+        return prominences
     }
 }
--- Tests/PeakSwiftTests/PeakSwiftTests.swift
+++ Tests/PeakSwiftTests/PeakSwiftTests.swift
@@ -182,6 +182,30 @@ final class PeakSwiftTests: XCTestCase {
         XCTAssertEqual(actualPeaks, expectedPeaks)
     }
     
+    func testFindPeakProminences() {
+        
+        let inputSignal: [Double] = [1,-8,1,5,-5,3,-9]
+        let peaks = [3,5]
+        
+        let actualProminence = PeakUtils.findAllPeakProminences(signal: inputSignal, peaks: peaks)
+        let expectedProminence: [Double] = [13, 8]
+        
+        XCTAssertEqual(actualProminence, expectedProminence)
+    }
+    
+    func testFindAllPeaksAndTheirProminences() {
+        
+        let inputSignal: [Double] = [1,-8,1,5,-5,3,-9]
+        
+        let actualPeaksAndProminence = PeakUtils.findAllPeaksAndProminences(signal: inputSignal)
+        
+        let expectedPeaks = [3,5]
+        let expectedProminence: [Double] = [13, 8]
+        
+        XCTAssertEqual(actualPeaksAndProminence.peakPosition, expectedPeaks)
+        XCTAssertEqual(actualPeaksAndProminence.peakProminences, expectedProminence)
+    }
+    
     
     func testButterworthOrder1() {
         let butterworth = Butterworth()
