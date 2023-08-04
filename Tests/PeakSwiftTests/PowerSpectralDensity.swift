a7851075f3652420aa3aeeb98bdfa094bb73d970
NikitaChar <nikita.charush@gmail.com>
2023-08-04 16:31:14 +0200

    feat(Tests): Add more real test

--- Tests/PeakSwiftTests/PowerSpectralDensity.swift
+++ Tests/PeakSwiftTests/PowerSpectralDensity.swift
@@ -29,4 +29,51 @@ final class PowerSpectralDensity: XCTestCase {
         AssertEqualWithThreshold(actualPSD.frequencies, expectedPSDFrequencies)
         AssertEqualWithThreshold(actualPSD.power, expectedPSDPower, threshold: Constants.doubleAccuracy)
     }
+    
+    func testWelchGeneratedSignal() {
+        
+        let sin50Hz = SinusComponent(amplitude: 0.7, frequency: 50)
+        let sin120Hz = SinusComponent(amplitude: 1, frequency: 120)
+        let samplingFrequency = 300.0
+        let signalLength = 2048
+        let nperseg = signalLength / 2
+        let signalGenerator = SignalGenerator(signalComponents: [sin50Hz, sin120Hz])
+
+        let signal = signalGenerator.synthesizeSignal(samplingFrequency: samplingFrequency, signalLength: signalLength)
+        
+        let actualPSD = Welch.estimatePowerSpectralDensity(signal: signal, samplingFrequency: samplingFrequency, nperseg: nperseg, noverlap: nil, nfft: signalLength)
+        
+        // There should be only 2 prominent peaks at frequencies 50Hz and 120Hz
+        let actualPeaks = PeakUtils.findAllPeaksAndProminences(signal: actualPSD.power, minProminence: 0.1).peakPosition
+        // Generated with neurokit
+        let expectedPeaks = [341, 819]
+        
+        XCTAssertEqual(actualPeaks, expectedPeaks)
+        
+    }
+    
+    func testWelchGeneratedSignalNoPow2() {
+        
+        let sin50Hz = SinusComponent(amplitude: 0.7, frequency: 50)
+        let sin120Hz = SinusComponent(amplitude: 1, frequency: 120)
+        let samplingFrequency = 300.0
+        let signalLength = 700
+        let nperseg = signalLength / 2
+        let signalGenerator = SignalGenerator(signalComponents: [sin50Hz, sin120Hz])
+
+        let signal = signalGenerator.synthesizeSignal(samplingFrequency: samplingFrequency, signalLength: signalLength)
+        //print(signal)
+        
+        let actualPSD = Welch.estimatePowerSpectralDensity(signal: signal, samplingFrequency: samplingFrequency, nperseg: nperseg, noverlap: nil, nfft: 1024)
+        print(actualPSD.frequencies)
+        
+        // There should be only 2 prominent peaks at frequencies 50Hz and 120Hz
+        let actualPeaks = PeakUtils.findAllPeaksAndProminences(signal: actualPSD.power, minProminence: 0.1).peakPosition
+        // Generated with neurokit
+        let expectedPeaks = [171, 410]
+        
+        XCTAssertEqual(actualPeaks, expectedPeaks)
+        
+    }
+    
 }
